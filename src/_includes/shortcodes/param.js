2a18684cbd6cef1f6da112631161f9f1a78c3aff
Adon Moskal <adon@cloudcannon.com>
2023-11-23 07:41:30 +0000

    Updated 1 file via CloudCannon.

---
+++ src/_includes/shortcodes/param.js
@@ -0,0 +1,7 @@
+const matter = require('gray-matter')
+
+module.exports = param_name => {
+  const str = fs.readFileSync(this.page.inputPath, 'utf8')
+  const data = matter(str).data
+  return data[param_name] ? data[param_name] : `Param ${param_name} not found.`
+}
