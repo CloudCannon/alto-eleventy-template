3a8d541f4e0814b3708eef626a2aab599a88e31c
James-cloudcannon <james@cloudcannon.com>
2023-12-12 09:06:51 +1300

    feat: updates to show highlight lines

--- src/_includes/shortcodes/diffcode.js
+++ src/_includes/shortcodes/diffcode.js
@@ -1,43 +1,53 @@
 const markdownIt = require("markdown-it");
-var utils = require('markdown-it/lib/common/utils');
-var hljs = require('highlight.js'); // https://highlightjs.org
+const utils = require('markdown-it/lib/common/utils');
+const hljs = require('highlight.js'); // https://highlightjs.org
 
 module.exports = content => {
-    let mainBlock = content.replace(/(^```.*)/mg, "")
+    let mainBlock = content.replace(/(^```.*)/mg, "");
     
-    let diff = mainBlock.split("\n").filter(x => x !== "").join("\n")
-    diff = diff.replace(/(?:^([+-]).*$)/mg, "$1")
-    diff = diff.replace(/(?:^((?!\+).*)$)/mg, ".")
-    diff = diff.replace(/\n/g, "")
+    let diff = mainBlock.split("\n").filter(x => x !== "").join("\n");
+    diff = diff.replace(/(?:^([+~\-]).*$)/mg, "$1"); 
+    diff = diff.replace(/(?:^((?![+\-~]).*)$)/mg, "."); 
+    diff = diff.replace(/\n/g, "");
 
-    let lines = diff.split("")   
+    let lines = diff.split("");
 
     var result = markdownIt({
-        html:true,
+        html: true,
         highlight: function (str, lang) {            
             if (lang && hljs.getLanguage(lang)) {
-              try {     
-                let highlight = hljs.highlight(str, { language: lang, ignoreIllegals: true });
-                highlight = highlight.value;    
-                let result = "";
-                highlight.split("\n").forEach((line, i) => {
-                    if(lines[i] === "+")
-                    {
-                        line = line.replace(/(^([+-]))/mg, "")
-                        result += `<span class="line" data-add="true"><span>${line}</span></span>`
-                    }
-                    else
-                        result += line + "\n"
-                })
-                
-                return `<pre class="chroma"><code class="" data-lang='${lang}'>` +
-                       result +
-                       '</code></pre>';
-              } catch (__) {}
+                try {     
+                    let highlight = hljs.highlight(str, { language: lang, ignoreIllegals: true });
+                    highlight = highlight.value;    
+                    let result = "";
+                    highlight.split("\n").forEach((line, i) => {
+                        line = line.replace(/(^([+\-~]))/mg, "");
+
+                        switch (lines[i]) {
+                            case "+":
+                                result += `<span class="line" data-add="true"><span>${line}</span></span>`;
+                                break;
+                            case "-":
+                                result += `<span class="line" data-remove="true"><span>${line}</span></span>`;
+                                break;
+                            case "~":
+                                result += `<span class="line" data-highlight="true"><span>${line}</span></span>`;
+                                break;
+                            default:
+                                result += line + "\n";
+                        }
+                    });
+                    
+                    return `<pre class="chroma"><code class="" data-lang='${lang}'>` +
+                           result +
+                           '</code></pre>';
+                } catch (__) {}
             }
         
             return '<pre class="chroma"><code class="">' + utils.escapeHtml(str) + '</code></pre>';
         }
-    }).render(content)
-    return result
-  }
+    }).render(content);
+
+    return result;
+};
+
